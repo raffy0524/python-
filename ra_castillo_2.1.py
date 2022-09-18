24714adf55178e26162ffd72c30c9a6820579435
raffy0524 <113395471+raffy0524@users.noreply.github.com>
2022-09-17 22:53:43 -0400

    Add files via upload

---
+++ ra_castillo_2.1.py
@@ -0,0 +1,9 @@
+#Rafael castillo
+#excersise 2.1
+#date: 06/12/2022
+
+print("RAFAEL CASTILLO")
+print("27 VERNDALE AVENUE PROVIDENCE RI 02905")
+print("401-588-9091")
+print("software developer")
+      
